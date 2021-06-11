63d5f06d8f66e9a49b03f2f40244a056987d6fca
Brian Riwu Kaho <b.riwukaho@gmail.com>
2021-06-11 10:09:42 +1000

    updated site layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,29 +6,56 @@ import {
   navLinks,
   navLinkItem,
   navLinkText,
+  navOwnerName,
+  headerContent,
+  header,
+  ownerInfoContainer,
+  professionSpan,
+  contentSection,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <main className={container}>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <h1 className={heading}>{pageTitle}</h1>
-      {children}
-    </main>
+    <div>
+      <header className={header}>
+        <div className={headerContent}>
+          <div className={ownerInfoContainer}>
+            <span className={navOwnerName}>Brian Riwu Kaho</span>
+            <span className={professionSpan}>Software Engineer</span>
+          </div>
+          <nav>
+            <ul className={navLinks}>
+              <li className={navLinkItem}>
+                <Link to="/" className={navLinkText}>
+                  Home
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/about" className={navLinkText}>
+                  About
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/blog" className={navLinkText}>
+                  Blog
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/contact" className={navLinkText}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className={container}>
+        <section className={contentSection}>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </section>
+      </main>
+    </div>
   );
 };
 
